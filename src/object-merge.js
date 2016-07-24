4f72a74ca10ea4d925c6a79a3ee7059632207c9e
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-24 01:56:49 -0500

    ObjectMerge class start

---
+++ src/object-merge.js
@@ -0,0 +1,10 @@
+export default class ObjectMerge {
+  constructor(original, replace) {
+    this.original = original;
+    this.replace = replace;
+  }
+
+  run() {
+
+  }
+}
--- test/object-merge-test.js
+++ test/object-merge-test.js
@@ -1,1 +1,15 @@
-var assert = require('chai').assert;
+var expect = require('chai').expect;
+var ObjectMerge = require('../dist/object-merge').default;
+
+describe('Object Merge', function() {
+  it('should create an ObjectMerger instance', function() {
+    var orig = {a: '1'};
+    var replace = {a: '1'};
+    var result = new ObjectMerge(orig, replace);
+
+    expect(result).to.be.an.instanceof(ObjectMerge);
+    expect(result.original).to.equal(orig);
+    expect(result.replace).to.equal(replace);
+    expect(result.run).to.be.a('function');
+  });
+});
